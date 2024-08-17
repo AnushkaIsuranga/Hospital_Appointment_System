bb29420cd8eb10eb2d260bc88b0f3624fb7a9306
aka_andy780 <144549139+AnushkaIsuranga@users.noreply.github.com>
2024-08-18 02:34:33 +0530

    Delete HospitalAppointmentSystem_API directory

--- HospitalAppointmentSystem_API/src/main/java/nibm/hdse241/hospitalappointmentsystem/repositories/appointmentRepository.java
+++
@@ -1,7 +0,0 @@
-package nibm.hdse241.hospitalappointmentsystem.repositories;
-
-import nibm.hdse241.hospitalappointmentsystem.entities.Appointment;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface appointmentRepository extends JpaRepository<Appointment, Integer> {
-}
