57fdf2b7f36c1f46b627f1e23d1747013f671c96
aka_andy780 <144549139+AnushkaIsuranga@users.noreply.github.com>
2024-08-18 03:10:21 +0530

    API

---
+++ HospitalAppointmentSystem_API/src/main/java/nibm/hdse241/hospitalappointmentsystem/model/appointmentModel.java
@@ -0,0 +1,12 @@
+package nibm.hdse241.hospitalappointmentsystem.model;
+
+public class appointmentModel {
+    private int id;
+    private String patientName;
+    private String doctorName;
+    private String patientMobile;
+    private String patientEmail;
+    private String appointmentTime;
+    private String appointmentDate;
+    private boolean appointmentStatus;
+}
